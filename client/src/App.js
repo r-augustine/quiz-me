1e96c2e9b23bbdd1f7056705cd928ebdfe0ce38f
Ricardo Augustine <augustineri93@gmail.com>
2021-08-16 02:18:30 -0400

    setup routing with react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { makeStyles } from "@material-ui/core/styles";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./features/nav-bar/Navbar";
+import Practice from "./features/practice/Practice";
+import Domain from "./features/domain/Domain";
+
+const useStyles = makeStyles({
+  root: {
+    background: "#f3f7f7",
+  },
+});
 
 function App() {
+  const classes = useStyles();
+  const [value, setValue] = useState(0);
+  const onNavChange = (event, value) => {
+    setValue(value);
+  };
   return (
-    <>
-      <Navbar />
-    </>
+    <Router className={classes.root}>
+      <Navbar value={value} onChange={onNavChange} />
+      <Switch>
+        <Route exact path="/" component={Practice} index={0} value={value} />
+        <Route path="/domains/:domain" component={Domain} />
+      </Switch>
+    </Router>
   );
 }
 
